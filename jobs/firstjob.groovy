40442d70b7d3c41c5da94b51e71c7949bef271ef
Cédric Cabessa <ccabessa@genymobile.com>
2016-02-13 19:44:44 +0100

    code

--- jobs/firstjob.groovy
+++ jobs/firstjob.groovy
@@ -1,3 +1,6 @@
+import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
+
 job('DSL-Tutorial-1-Test') {
     description("""This a wonderful project
 With a multiline description""")
@@ -21,6 +24,40 @@ echo 'hello world'
         archiveArtifacts('output')
     }
 }
+
+class JobBuilder {
+    String name
+    String description
+    String giturl
+    String gitBranch = 'master'
+    String pollScmSchedule = 'H/5 * * * *'
+
+    Job build(DslFactory dslFactory) {
+        dslFactory.job(name) {
+            it.description this.description
+            logRotator {
+                numToKeep 5
+            }
+            scm {
+                git {
+                    remote {
+                        url(giturl)
+                        branch(gitBranch)
+                    }
+                }
+            }
+            triggers {
+                scm pollScmSchedule
+            }
+        }
+    }
+}
+
+new JobBuilder(
+        name: "myjobbuild job",
+        description: "descri",
+        giturl: "git@github:Plop",
+).build(this)
 
 job('myjob') {
     description("""A cool description""")
