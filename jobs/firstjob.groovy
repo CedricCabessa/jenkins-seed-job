7956a86dd2eba5f563a09a4f246b46f7a8d564ac
Cédric Cabessa <ccabessa@genymobile.com>
2016-02-13 20:06:40 +0100

    factorize

--- jobs/firstjob.groovy
+++ jobs/firstjob.groovy
@@ -1,5 +1,3 @@
-import javaposse.jobdsl.dsl.DslFactory
-import javaposse.jobdsl.dsl.Job
 
 job('DSL-Tutorial-1-Test') {
     description("""This a wonderful project
@@ -25,65 +23,14 @@ echo 'hello world'
     }
 }
 
-class JobBuilder {
-    String name
-    String description
-    String giturl
-    String gitBranch = 'master'
-    String pollScmSchedule = 'H/5 * * * *'
 
-    Job build(DslFactory dslFactory) {
-        dslFactory.job(name) {
-            it.description this.description
-            logRotator {
-                numToKeep 5
-            }
-            scm {
-                git {
-                    remote {
-                        url(giturl)
-                        branch(gitBranch)
-                    }
-                }
-            }
-            triggers {
-                scm pollScmSchedule
-            }
-        }
-    }
-}
 
-new JobBuilder(
-        name: "myjobbuild job",
-        description: "descri",
-        giturl: "git@github:Plop",
+new MasterMatrixBuilder(
+        name: "mynew job",
+        description: "awesome job",
+        giturl: "http://giturl",
+        buildScript: """
+./build.sh
+echo "plop"
+"""
 ).build(this)
-
-job('myjob') {
-    description("""A cool description""")
-    scm {
-        git("git://github.com/foo/bar")
-    }
-}
-
-
-matrixJob("my multi job") {
-    axes {
-        label("label", "linnode", "winnode", "macnode")
-    }
-    scm {
-        git {
-            remote {
-                url('git://github.com/CedricCabessa/lightduino')
-                branch('${sha1}')
-                refspec("+refs/pull/*:refs/remotes/origin/pr/*")
-            }
-        }
-    }
-    triggers {
-        pullRequest {
-            cron('H/5 * * * *')
-            permitAll()
-        }
-    }
-}
---
+++ src/main/groovy/genymobile/jenkins/jenkins.groovy
@@ -0,0 +1,46 @@
+import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
+import javaposse.jobdsl.dsl.jobs.MatrixJob
+
+class MasterMatrixBuilder {
+    String name
+    String description
+    String giturl
+    String buildScript
+    String archiveArtifacts
+    String gitBranch = 'master'
+    String pollScmSchedule = 'H/5 * * * *'
+    int artefactToKeep = 10
+
+    MatrixJob build(DslFactory dslFactory) {
+        dslFactory.matrixJob(name) {
+            it.description this.description
+            logRotator {
+                numToKeep artefactToKeep
+            }
+            scm {
+                git {
+                    remote {
+                        url(giturl)
+                        branch(gitBranch)
+                        credentials("user")
+                    }
+                    clean()
+                }
+
+            }
+            triggers {
+                scm pollScmSchedule
+            }
+            axes {
+                label("label", "linnode", "winnode", "macnode")
+            }
+            steps {
+                shell(buildScript)
+            }
+            publishers {
+                archiveArtifacts(archiveArtifacts)
+            }
+        }
+    }
+}
